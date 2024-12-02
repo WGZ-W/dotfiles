8021be0951cddabd8996fe01dec531b2ff178975
wgz <371960029@qq.com>
2024-12-02 17:37:02 +0800

    add nvim configuration

---
+++ nvim/lua/plugins/cmp.lua
@@ -0,0 +1,77 @@
+return { -- Autocompletion
+	"hrsh7th/nvim-cmp",
+
+	dependencies = {
+		-- Snippet Engine & its associated nvim-cmp source
+		"L3MON4D3/LuaSnip",
+		"saadparwaiz1/cmp_luasnip",
+
+		-- Adds other completion capabilities.
+		--  nvim-cmp does not ship with all sources by default. They are split
+		--  into multiple repos for maintenance purposes.
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-buffer", -- buffer auto-completion
+		"hrsh7th/cmp-cmdline", -- cmdline auto-completion
+	},
+
+	config = function()
+		-- See `:help cmp`
+		local cmp = require("cmp")
+		local luasnip = require("luasnip")
+		luasnip.config.setup({})
+
+		-- Autopairs
+		-- If you want insert '(' after select function or method item.
+		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+		cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+
+		-- Set up lspconfig.
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+		-- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+		require("lspconfig")["<YOUR_LSP_SERVER>"].setup({
+			capabilities = capabilities,
+		})
+
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					luasnip.lsp_expand(args.body)
+				end,
+			},
+			completion = { completeopt = "menu,menuone,noinsert" },
+
+			-- For an understanding of why these mappings were
+			-- chosen, you will need to read `:help ins-completion`
+			--
+			-- No, but seriously. Please read `:help ins-completion`, it is really good!
+			mapping = cmp.mapping.preset.insert({
+				-- Select the [n]ext item
+				["<Tab>"] = cmp.mapping.select_next_item(),
+				-- Select the [p]revious item
+				["<S-Tab>"] = cmp.mapping.select_prev_item(),
+
+				-- Scroll the documentation window [b]ack / [f]orward
+				["<C-b>"] = cmp.mapping.scroll_docs(-4),
+				["<C-f>"] = cmp.mapping.scroll_docs(4),
+				["<C-Space>"] = cmp.mapping.complete(),
+				["<CR>"] = cmp.mapping.confirm({ select = true }),
+
+				-- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
+				--    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+			}),
+
+			sources = {
+				{
+					name = "lazydev",
+					-- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
+					group_index = 0,
+				},
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" },
+				{ name = "path" },
+				{ name = "buffer" }, -- For buffer word completion
+			},
+		})
+	end,
+}
